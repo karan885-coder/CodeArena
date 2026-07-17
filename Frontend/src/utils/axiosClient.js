20ca8e262e9cd8fb56a5b9d5a4856cf8f87679b4
Karan Kumar <outlierkaran786@gmail.com>
2026-07-17 11:52:41 +0530

    Update frontend API base URL

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:3000',
+    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://codearena-production-1e7e.up.railway.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
